e5a5f0897b07bc7463b89f841b543c2ea1eda9c6
tientien1103 <le.nttien99@gmail.com>
2023-03-22 21:24:08 +0700

    solution

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,10 +1,14 @@
 import React from "react";
 
-function Result({ "Your code here" }) {
+// props (properties)
+function Result(props) {
+  const { user1GameItem, user2GameItem, result } = props;
+
   return (
     <div>
       <h1 className="result-header">Result</h1>
       <div className="result-wrapper">
+        {/* conditional rendering */}
         {user1GameItem && <span>{user1GameItem.name}</span>}
         <span className="result">{result}</span>
         {user2GameItem && <span>{user2GameItem.name}</span>}
